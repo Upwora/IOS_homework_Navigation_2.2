60368fe7caba02fdf11e67be92d1a2424d47801f
Oleg <ovoloshin@gmail.com>
2022-06-20 07:41:59 +0300

    Bug with animation fixed.

--- Navigation/PhotoGallery/PhotosTableViewCell.swift
+++ Navigation/PhotoGallery/PhotosTableViewCell.swift
@@ -96,10 +96,10 @@ let gallery = PhotosViewController()
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
 
 //    override func setSelected(_ selected: Bool, animated: Bool) {
 //        super.setSelected(selected, animated: animated)
